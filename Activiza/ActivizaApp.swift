bce99f13d903faa7dce5244fd95206a57dba4d5c
nestorsan77 <nestorsan77@gmail.com>
2024-06-17 09:40:31 +0200

    Initial Commit

---
+++ Activiza/ActivizaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivizaApp.swift
+//  Activiza
+//
+//  Created by Nestor Calderon perez on 17/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ActivizaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
